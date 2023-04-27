75d85a8ef9767e4ec97175afc8a541059a70dac5
dorgold46 <dgoldenberg@lightricks.com>
2023-04-27 15:54:50 +0300

    first commit

---
+++ src/get_data.py
@@ -0,0 +1,137 @@
+import pandas as pd
+from google.cloud import bigquery
+import yaml
+import typing
+import pathlib
+import os.path
+
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
+
+
+class SingleQuery:
+    """
+    A class for running a single query on BigQuery.
+
+    Args:
+        path_to_query (str or pathlib.Path): The path to the SQL file containing the query.
+
+    Attributes:
+        client (google.cloud.bigquery.client.Client): A client object for interacting with BigQuery.
+        path (str or pathlib.Path): The path to the SQL file containing the query.
+        query (str): The contents of the SQL file.
+
+    Methods:
+        run_query(result: str) -> Union[pd.DataFrame, None]:
+            Runs the query on BigQuery and returns the result as a DataFrame or None.
+        post_process(df: pd.DataFrame) -> pd.DataFrame:
+            A static method for performing post-processing on the query result.
+
+    Example:
+        >>> from src.get_data import SingleQuery
+        >>> query = SingleQuery('queries/templates_with_features.txt.txt')
+        >>> result = query.run_query('df')
+        >>> processed_result = SingleQuery.post_process(result)
+    """
+
+    def __init__(self, path_to_query: typing.Union[str, pathlib.Path]):
+        """
+        Initializes a new SingleQuery object.
+
+        Args:
+            path_to_query (str or pathlib.Path): The path to the SQL file containing the query.
+        """
+        self.client = bigquery.Client(location=config['cloud']['CLOUD_LOCATION'],
+                                      project=config['cloud']['PROJECT'])
+        self.path = path_to_query
+        with open(self.path, "r") as f:
+            self.query = f.read()
+
+    def run_query(self, result: str) -> typing.Union[pd.DataFrame, None]:
+        """
+        Runs the query on BigQuery and returns the result as a DataFrame or None.
+
+        Args:
+            result (str): Either "df" to return a DataFrame or "bq" to just run the query.
+
+        Returns:
+            Union[pd.DataFrame, None]: If result is "df", returns the query result as a pandas DataFrame.
+                If result is "bq", returns None.
+
+        Raises:
+            ValueError: If result is not "df" or "bq".
+        """
+        query_job = self.client.query(
+            self.query,
+            location=config['cloud']['CLOUD_LOCATION'],
+        )
+        if result == "bq":
+            query_job.result()
+        elif result == "df":
+            df = query_job.to_dataframe()
+            df = self.post_process(df)
+            return df
+        else:
+            raise ValueError(
+                'result must be either "bq" for running BigQuery action or "df" for returning DataFrame'
+            )
+
+    @staticmethod
+    def post_process(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        A static method for performing post-processing on the query result. Can be override
+
+        Args:
+            df (pd.DataFrame): The query result as a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: The processed DataFrame.
+        """
+        return df
+
+
+def fetch_data(query_path, data_path, reload=False, pp_function=None):
+    """
+            This function checks if the tags data exists, if not it queries it
+            :return: DataFrame. df of tags
+            """
+    is_exist = os.path.isfile(data_path)
+    if is_exist and not reload:
+        df = pd.read_csv(data_path, header=0)
+        return df
+    else:
+        sq = SingleQuery(path_to_query=query_path)
+        if pp_function is not None:
+            sq.post_process = pp_function
+        df = sq.run_query(result="df")
+        df.to_csv(data_path)
+        return df
+
+
+def main(reload=True):
+    def _post_process(df):
+        import ast
+        import re
+
+        def _parse_lists(x):
+            x = str(x)
+            x = re.sub(r'(?<=\[)\s+', '', x)
+            x = re.sub(r' +', ',', x)
+            x = re.sub(r'\n', '', x)
+            x = ast.literal_eval(x)
+            return x
+
+        new_df = df
+        new_df['feature_types'] = new_df['feature_types'].apply(_parse_lists)
+        new_df['start_times'] = new_df['start_times'].apply(_parse_lists)
+        new_df['feature_durations'] = new_df['feature_durations'].apply(_parse_lists)
+
+        new_df = new_df.explode(['feature_types', 'start_times', 'feature_durations'])
+        return new_df
+
+    fetch_data(config['paths']['TEMPLATES_QUERY_PATH'], config['paths']['TEMPLATES_PATH'], pp_function=_post_process,
+               reload=reload)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/video_utils.py
@@ -0,0 +1,99 @@
+import os
+import random
+import re
+
+import cv2
+import pandas as pd
+import yaml
+from torchvision.datasets.utils import download_url
+
+
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
+VIDEOS_PATH = config['paths']['VIDEOS_PATH']
+
+
+def get_urls(num_samples=None):
+    urls = pd.read_csv('data/originals_ids.csv')
+    urls = urls.iloc[50_000:]
+    urls = urls.media_id.tolist()
+    if num_samples is not None:
+        urls = random.sample(urls, num_samples)
+    base_url = "https://res.cloudinary.com/lightricks/video/upload/"
+    urls = [base_url + s + '.mp4' for s in urls]
+    return urls
+
+
+def download_video(url, destination="data/video_data"):
+    extenstion = re.sub("https://res.cloudinary.com/lightricks/video/upload/", "", url)
+    download_url(
+        url,
+        destination,
+        extenstion
+    )
+
+
+def download_multiple_videos(url_list):
+    for url in url_list:
+        download_video(url)
+
+
+def delete_video(video_path):
+    os.remove(video_path)
+
+
+def delete_multiple_videos(videos_path):
+    for video_path in videos_path:
+        delete_video(video_path)
+
+
+def get_existing_path():
+    files = os.listdir(VIDEOS_PATH)
+    paths = [VIDEOS_PATH + i for i in files]
+    return paths
+
+
+def load_video(video_path):
+    video = cv2.VideoCapture(video_path)
+    return video
+
+
+def load_multiple_videos(videos_path):
+    videos = []
+    for video_path in videos_path:
+        video = load_video(video_path)
+        videos.append(video)
+    return videos
+
+
+def build_dataset(num_samples):
+    # TODO: check for duplications
+    urls = get_urls(num_samples)
+    download_multiple_videos(urls)
+
+
+def save_encoding(frames_df, save_path, visited_path):
+    names = frames_df['name'].drop_duplicates()
+
+    if not os.path.isfile(visited_path):
+        names.to_csv(visited_path)
+    elif os.path.isfile(visited_path):
+        names = names.tolist()
+        visited = pd.read_csv(visited_path)
+        visited = visited['name'].tolist()
+        set_a = set(visited)
+        set_b = set(names)
+        difference = set_b - set_a
+        names = pd.DataFrame({'name': list(difference)})
+        names.to_csv(visited_path, mode='a', header=False)
+
+    if not os.path.isfile(save_path):
+        frames_df.to_csv(save_path)
+    elif os.path.isfile(save_path) and len(names) > 0:
+        frames_df.to_csv(save_path, mode='a', header=False)
+    elif len(names) == 0:
+        print("no new video was found")
+
+
+if __name__ == '__main__':
+    build_dataset(10)
